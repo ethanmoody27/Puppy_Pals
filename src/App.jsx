a3a2d65710a6ca96b0be4b06b920ecf30a1cd261
ethanmoody27 <ethanmoody27@gmail.com>
2023-10-11 08:55:53 -0400

    Final commit with all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import { puppyList } from './data.js';
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("Featured Puppy: ", featuredPup);
+
   return (
-    <div>
-      {/* Empty page */}
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
+
